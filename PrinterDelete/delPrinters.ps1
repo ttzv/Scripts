f08b35572b4fa3408a6853c2a81d750d9e76b212
Tomasz Żwak <tomasz.zwak@gmail.com>
2021-03-11 15:43:42 +0100

    Add PrinterDelete script

---
+++ PrinterDelete/delPrinters.ps1
@@ -0,0 +1,22 @@
+﻿$plist = Get-Printer
+Write-Host "Installed printers"
+$i = 1
+Foreach ($p in $plist.Name) {
+    Write-Host $i'. '$p
+    $i++
+}
+$input = Read-Host -Prompt 'Printer numbers that should be deleted (separate using spaces)'
+$inpArr = $input -Split ' '
+if($inpArr){
+$inpArr.Length
+$j
+    Foreach($j in $inpArr) {
+        $pName = $plist.Name[$j-1]
+        Write-Host 'Deleting printer'$pName
+        Remove-Printer -Name $pName
+    }
+} else {
+    Write-Host "Number not provided."
+}
+
+
---
+++ PrinterDelete/delPrintersRun.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Optional launcher for powershell script, path below assumes the script is
+REM in the same folder as this launcher.
+powershell.exe -ExecutionPolicy Bypass -File delPrinters.ps1
+pause
