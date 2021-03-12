96cba329e9258c44999f26f51ecdd62261a38c4b
Tomasz Żwak <tomasz.zwak@gmail.com>
2021-03-12 15:14:08 +0100

    add ChangePasswordAtLogon Script

---
+++ Change password at logon/ChangePasswordAtLogon.ps1
@@ -0,0 +1,5 @@
+﻿# Asks for user sAMAccountName and makes it so the user has to
+# change their password when they log in for the first time.
+
+$uName = Read-Host -Prompt "User"
+Set-ADUser -ChangePasswordAtLogon $true -Identity $uName -Confirm:$false -Verbose
---
+++ Change password at logon/runChangePasswordAtLogon.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Optional .bat launcher
+powershell.exe -ExecutionPolicy Bypass -File ChangePasswordAtLogon.ps1
